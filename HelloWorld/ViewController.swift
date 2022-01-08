06d9bac9e0250156b147c2c29c035e476680aefc
Ula <1295593@mail.ru>
2022-01-08 22:15:23 +0800

    all

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var textButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .green
+        textButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func getTextButtonPress() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            textButton.setTitle("Text", for: .normal)
+        } else {
+            textButton.setTitle("No text", for: .normal)
+        }
+    }
+    
 }
 
